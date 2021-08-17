ca5c9b83b8e1d4fc37c0d6c11df0edaa7f30031c
Pablo Castillo <psc88sdrd@gmail.com>
2021-08-17 18:33:09 -0300

    scripts de alert, uso de document, suma y resta

--- js/holaMundo.js
+++ js/holaMundo.js
@@ -1,1 +1,37 @@
-alert("Hola mundo")
+//esto  es un comentario simple
+
+/*
+esto es
+un comentario
+de multiples lineas 
+*/
+
+//mostrar un mensaje por consola
+console.log('hola comision 17');
+  
+//mostrar un mensaje en la pagina html
+document.write('Este es mi primer mensaje piolon')
+
+// mostrar una ventana emergente
+//alert('esto es una alerta')
+
+// crear o declarar variables var(puede cambiar su valor) - let(puede cambiar su valor) - const(valores fijos)
+let palabraBuscada = 'Pablo';
+
+document.write('<br>Variable palabra buscada: ' + palabraBuscada + ' como estas ' + palabraBuscada);
+
+//modificar el valor de una variable
+palabraBuscada = 'pagina de blog'
+
+document.write('<br>Variable palabra buscada: ' + palabraBuscada);
+
+const comision = 17;
+document.write('<br>Consante comision: ' + comision);
+
+//valor del tipo bool
+let luz = false;
+
+let suma = 20;
+console.log(--suma);
+console.log(suma +1);
+
