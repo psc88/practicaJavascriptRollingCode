98946c979935916947de7e62882ff810333260f6
Pablo Castillo <psc88sdrd@gmail.com>
2021-08-31 20:59:46 -0300

    Agregado de pountos 5 6 7 8 y 9

---
+++ js/arreglos.js
@@ -0,0 +1,17 @@
+anime = 'Dragon Ball Z';
+
+// crear o inicializar un arreglo []
+let series = ['Dragon Ball', 'Naruto', 'Evangelion 3.0+1', anime, 15, true];
+// crear un arreglo vacio
+let temporadas = [];
+
+// cuando quiero acceder a un elemento del arreglo nombrearreglo[posicion]
+document.write('Serie: '+ serie[3] );
+
+//agregar un elemento a un arreglo
+series.push('Loky');
+series.push('Breaking bad');
+
+console.log(series[6]);
+
+//
--- js/ejercicioBucles.js
+++ js/ejercicioBucles.js
@@ -9,6 +9,8 @@
 //   document.write("Ingreso un numero incorrecto");
 // }
 
+//----------------------------------------------------------------------------------------
+
 /*2- Escribir un programa que solicite una nota (número) de 0  a 10. Luego mostrar la calificación en un alert según los siguientes rangos de nota:*/
 // let nota = parseInt(prompt('Ingresa la nota del alumno: '));
 
@@ -30,6 +32,8 @@
 //     alert('Introduce un número valido')
 // )
 
+//----------------------------------------------------------------------------------------
+
 /*3- Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.*/
 
 // let r = true;
@@ -47,6 +51,8 @@
 // }
 
 // document.write(cadena);
+
+//----------------------------------------------------------------------------------------
 
 /*4- Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un número deberá indicarse con un «alert» y seguir pidiendo números. Al salir con “cancelar” deberá indicarse la suma total de los números introducidos.*/
 
@@ -67,6 +73,8 @@
 
 // document.write('el total es: ',resultado);
 
+//----------------------------------------------------------------------------------------
+
 // 5- Realizar una página con un script que calcule el valor de la letra de un número de DNI (Documento nacional de identidad).
 
 // do{
@@ -81,32 +89,94 @@
 //     }
 // }while(confirm('Ingresar otro dni ?'))
 
+//----------------------------------------------------------------------------------------
+
+// 6- Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
+
+// let numeros = 30;
+
+// for(let i=1; i<=numeros; i++){
+//     for(let multiplicador=0; multiplicador<i; multiplicador++){
+//         document.write(i)
+//     }
+//     document.write('<br>')
+// }
+
+//----------------------------------------------------------------------------------------
+
+// 7- Haz un script que escriba una pirámide inversa de los números del 1 al número que indique el usuario (no mayor de 50)  de la siguiente forma : (suponiendo que indica 30).
+
+// let numero = parseInt(prompt("Ingrese un numero de referencia: "));
+
+// if(numero > 0 && numero <= 50) {
+//   for (let i = numero; i >= 1; i--) {
+//     for (let multiplicador = i; multiplicador >=1; multiplicador--) {
+//         document.write(i);
+//     }
+//     document.write("<br>");
+//   }
+// } else {
+//     document.write('el numero no esta dentro de los parametros')
+// }
+
+//----------------------------------------------------------------------------------------
+
+// 8- Crea script para generar pirámide siguiente con los números del 1 al número que indique el usuario (no mayor de 50) 
+
+// let numero = parseInt(prompt('Ingrese un numero: '));
+
+// if(numero > 0 && numero <= 50){
+//     for(let i=0; i<numero; i++){
+//         for(let j=1; j<i+2; j++){
+//             console.log(j)
+//             document.write(j)
+//         }
+//         document.write('<br>')
+//     }
+// }
+
+//----------------------------------------------------------------------------------------
+
+// 9- Crea un script que escriba los números del 1 al 500, que indique cuáles son múltiplos de 4 y de 9 y que cada 5 líneas muestre una línea horizontal. Por ejemplo :
+
+// for(let i=1; i<=500; i++){
+//     document.write(i);
+//     if(i % 4 === 0){
+//         document.write('(multiplo de 4)')
+//     } else if(i % 9 === 0){
+//         document.write('(multiplo de 9)')
+//     }
+//     if(i % 5 === 0){
+//         document.write ('<hr>')
+//     }
+//     document.write('<br>')
+// }
+
+//----------------------------------------------------------------------------------------
+
 // 10- Realiza un script que pida número de filas y columnas y escriba una tabla. Dentro cada una de las celdas deberá escribirse un número consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
 
-let filas = parseInt(prompt('Ingrese un numero de filas: '));
-let columna = parseInt(prompt('Ingrese un numero de columnas: '));
-let total = filas * columna;
-// total valor para cargar las celdas de la tabla
+// let filas = parseInt(prompt('Ingrese un numero de filas: '));
+// let columna = parseInt(prompt('Ingrese un numero de columnas: '));
+// let total = filas * columna;
+// // total valor para cargar las celdas de la tabla
 
-document.write('<table border>');
+// document.write('<table border>');
 
-// document.write('<tr>');
-// document.write('<td>prueba');
-// document.write('</td>');
-// document.write('</tr>');
-for(let indiceFila = 0; indiceFila < filas; indiceFila++){
-    //dibujo la fila
-    document.write('<tr>');
-    //dibujo la columna
-    for(let indiceColumna = 0; indiceColumna < columna; indiceColumna++){
-        document.write('<td>'+ total);
-        document.write('</td>');
-        total--; //total = total-1;
-    }
-    document.write('</tr>');
-}
+// // document.write('<tr>');
+// // document.write('<td>prueba');
+// // document.write('</td>');
+// // document.write('</tr>');
+// for(let indiceFila = 0; indiceFila < filas; indiceFila++){
+//     //dibujo la fila
+//     document.write('<tr>');
+//     //dibujo la columna
+//     for(let indiceColumna = 0; indiceColumna < columna; indiceColumna++){
+//         document.write('<td>'+ total);
+//         document.write('</td>');
+//         total--; //total = total-1;
+//     }
+//     document.write('</tr>');
+// }
 
-document.write('</table>');
-
-
-
+// document.write('</table>');
