1bfdcdd0a606c31dfbaa831d960a1c9b7ffa1296
Pablo Castillo <psc88sdrd@gmail.com>
2021-09-08 23:57:25 -0300

    Actualizacion teoria funcion flecha en archivo 6-funciones.js

--- js/6-funciones.js
+++ js/6-funciones.js
@@ -8,21 +8,20 @@ function aumentarPrecio(){
 
 // funcion con parametros
 function actualizarPrecio(monto, producto, precio){
-    let calculo = precio + monto;
+    let calculo = monto * precio;
     document.write('<br>El producto: '+producto+' precio total $'+calculo)
 }
 
 // funciones que devuelven un valor
-    function convertirDolaresPesos(precioDolar){
-        let total = precioDolar * 184;
-        return total;
+    // function convertirDolaresPesos(precioDolar){
+    //     let total = precioDolar * 184;
+    //     return total;
         //jamas escribimos en esta zona, la linea de return es la ultima que se ejecuta
-    }
-
+    // }
 
 // funciones en forma de flecha =>
-let convertirDolaresPesos = (precioDolar) => {
-    let total = precioDolar * 184;
+let convertirDolaresPesos = (precioDolar, dolarBlue) => {
+    let total = precioDolar * dolarBlue;
     return total;
 }
 
@@ -34,12 +33,9 @@ let coeficienteInflacion = parseInt(prompt('Ingrese el monton total de la inflac
 actualizarPrecio(coeficienteInflacion, 'Teclado Hyperx', 9000);
 actualizarPrecio(coeficienteInflacion, 'Teclado Logitec', 14000);
 actualizarPrecio('mouse logitec',9000, 100);
-let monto = 3000;
-console.log(monto)
 
-if(10<coeficienteInflacion){
-    let prueba = 10;
-} else {
-    prueba 
-}
+let dolarBlue = parseInt(prompt('Ingrese el valor del dolar blue a la fecha'));
+let iphone = convertirDolaresPesos(1200, dolarBlue);
 
+document.write(`<br> Producto Iphone - precio $ ${iphone} (producto nuevo) `);
+document.write('<br> Podructo PS5 - precio $' +convertirDolaresPesos(500, dolarBlue)+'(ultima version)');
